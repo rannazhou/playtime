1c6899daf92455f26c4b62d2bee3f0a329af6c98
Jen Liu <jkliu@mit.edu>
2014-04-16 23:08:04 -0400

    working faux URL

--- playtime/app/urls.py
+++ playtime/app/urls.py
@@ -5,7 +5,7 @@ import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='home'),
 	url(r'^events', views.show_events, name='events'),
-	url(r'^events-plus', views.events_plus, name='events-plus'), 	#fake response
+	url(r'^new_events', views.events_plus, name='events-plus'), 	#fake response
     url(r'^peers', views.show_peers, name='peers'),
     url(r'^plan', views.plan, name='plan'),
 )

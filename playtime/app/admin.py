3a33e4e05ce360d4a994c86b08fb33bfdf0f6e7b
Jen Liu <jkliu@mit.edu>
2014-05-06 21:46:48 -0400

    more useful info on admin page

--- playtime/app/admin.py
+++ playtime/app/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from playtime.app.models import Buddy, Event, Group
 
-admin.site.register(Buddy)
-admin.site.register(Event)
-admin.site.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name']
+
+class BuddyAdmin(admin.ModelAdmin):
+	list_display = ['id', 'first_name', 'last_name']
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ['id', 'title']
+
+
+admin.site.register(Buddy, BuddyAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Group, GroupAdmin)
